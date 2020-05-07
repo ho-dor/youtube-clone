143f3f195873b37a35aece703b262ee4fcb3b35a
Kunal <kunalrai.cse16@nituk.ac.in>
2020-05-07 14:49:20 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Grid } from '@material-ui/core';
+
+function App() {
+    return (
+        <React.Fragment>
+            <Grid>
+                {/*Search*/}
+            </Grid>
+            <Grid>
+                {/*Video*/}
+            </Grid>
+            <Grid>
+                {/*VideoList*/}
+            </Grid>
+        </React.Fragment>
+  );
+}
+
+export default App;
